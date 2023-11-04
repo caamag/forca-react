7742c6451a9e1cfea9778883f897dd95e9413289
Caio Lopes <caiolopesfv@gmail.com>
2023-11-04 19:13:35 -0300

    enviando dados iniciais

---
+++ forca/src/data/words.js
@@ -0,0 +1,10 @@
+
+export const wordList = {
+
+    carro: ['Motor', 'Combustível', 'Parachoque', 'Pneu', 'Retrovisor', 'Capô'], 
+    fruta: ['Goiaba', 'Limão', 'Melancia', 'Laranja', 'Pera', 'Kiwi', 'Maçã', 'Uva'], 
+    computador: ['Armazenamento', 'Processador', 'Teclado', 'Mouse', 'Monitor'],
+    cor: ['Azul', 'Amarelo', 'Ciano', 'Vermelho', 'Branco', 'Preto', 'Salmão', 'Verde'], 
+    instrumentos: ['Violão', 'Piano', 'Guitarra', 'Cajon', 'Violino', 'Bateria', 'Flauta']
+
+}
